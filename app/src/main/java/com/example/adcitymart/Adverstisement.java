92c116d0787f03245b32552be73c7eba6821090d
Garry <geetanshsharmahp@gmail.com>
2019-06-13 11:28:20 +0530

    13/6/19

--- app/src/main/java/com/example/adcitymart/Adverstisement.java
+++ app/src/main/java/com/example/adcitymart/Adverstisement.java
@@ -1,14 +1,27 @@
 package com.example.adcitymart;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Adverstisement extends AppCompatActivity
 {
 
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adverstisement);
+
     }
 }
