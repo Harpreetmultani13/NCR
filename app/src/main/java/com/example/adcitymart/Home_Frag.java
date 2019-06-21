ab79916f1082a0c697c6a2f1c8c46fe4a020a858
harpreetmultani9614@gmail.com <harpreetmultani9614@gmail.com>
2019-06-21 01:15:15 -0700

    Harpreet Singh : re

--- app/src/main/java/com/example/adcitymart/Home_Frag.java
+++ app/src/main/java/com/example/adcitymart/Home_Frag.java
@@ -12,6 +12,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
